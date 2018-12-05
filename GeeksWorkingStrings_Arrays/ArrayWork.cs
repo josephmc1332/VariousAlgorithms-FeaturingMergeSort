4b88d82dd14185ce8b20ed11c83db8c37cdc6dbd
Joseph Callahan <Joseph.callahan1@outlook.com>
2018-12-05 00:21:44 -0500

    Adds methods to provide a solution to some common interview questions.

---
+++ GeeksWorkingStrings_Arrays/ArrayWork.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeeksWorkingStrings_Arrays
+{
+    class ArrayWork
+    {
+        public void StringReverse(string s)
+        {
+            int j = 0;
+            int len = 1;
+            Stack<string> stack = new Stack<string>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != '.')
+                {
+                    len++;
+                }
+                else
+                {
+                    stack.Push(s.Substring(j, len));
+                    if (i != s.Length - 1)
+                    {
+                        j = i + 1;
+                    }
+                }
+            }
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item + ".");
+            }
+        }
+
+
+        public void Merge(int[] arr, int l, int m, int r)
+        {
+            int n1 = m - l + 1;
+            int n2 = r - m;
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+            int i = 0;
+            int j = 0;
+            for (i = 0; i < n1; ++i)
+            {
+                L[i] = arr[l + i];
+            }
+            for (j = 0; j < n2; ++j)
+            {
+                R[j] = arr[m + 1 + j];
+            }
+            i = 0;
+            j = 0;
+            int k = l;
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    arr[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+            while (i < n1)
+            {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+            while (j < n2)
+            {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+        public void Sort(int l, int r, int[] arr)
+        {
+            if (l < r)
+            {
+                int m = (l + r) / 2;
+
+                Sort(l, m, arr);
+                Sort(m + 1, r, arr);
+
+                Merge(arr, l, m, r);
+            }
+        }
+
+
+        public int FindTriplets(int [] arr, int sum)
+        {
+            BubbleSort(ref arr);
+            int i = 0;
+            int k = arr.Length - 1;
+            int j = 1;
+            int answer = 0;
+            while(j < k)
+            {
+                if(arr[i] + arr[j] + arr[k] > sum)
+                {
+                    k--;
+                }
+                else
+                {
+                    answer += k - j;
+                    j++;
+                }
+            }
+            return answer;
+        }
+        public int LongestSequence(int [] arr)
+        {
+            int i = 0;
+            int count = 1;
+            int seq = int.MinValue;
+            BubbleSort(ref arr);
+            while(i < arr.Length - 1)
+            {
+                if (count > seq)
+                {
+                    seq = count;
+                }
+                if (arr[i] +1 != arr[i + 1])
+                {
+                    count = 1;
+                }
+                if (arr[i] + 1 == arr[i + 1])
+                    count++;
+                i++;
+            }
+            return seq;
+        }
+        public int [] BubbleSort(ref int [] arr)
+        {
+            bool flag = true;
+            for (int i = 0; i < arr.Length -1 && flag; i++)
+            {
+                flag = false;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if(arr[i] > arr[j])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                        flag = true;
+                    }
+
+                }
+            }
+            return arr;
+        }
+
+    }
+    
+}
--- GeeksWorkingStrings_Arrays/Program.cs
+++ GeeksWorkingStrings_Arrays/Program.cs
@@ -6,10 +6,110 @@ using System.Threading.Tasks;
 
 namespace GeeksWorkingStrings_Arrays
 {
-    class Program
+    class Program 
     {
         static void Main(string[] args)
         {
+            string arr = "Hello.my.name.is.joe";
+            ArrayWork array = new ArrayWork();
+            array.StringReverse(arr);
+            
+
+
+        }
+
+    }
+    class StringWork
+    {
+        public static string SwapString(string str)
+        {
+            char[] a = str.ToCharArray();
+            int i = 0;
+            int l = a.Length - 1;
+
+            while (i < l)
+            {
+                if (!char.IsLetter(a[i]))
+                    i++;
+                if (!char.IsLetter(a[l]))
+                    l--;
+                else
+                {
+                    char temp = a[i];
+                    a[i] = a[l];
+                    a[l] = temp;
+                    i++;
+                    l--;
+                }
+            }
+            string reply = new string(a);
+            return reply;
+        }
+        public static bool IsPalindrome(string arr)
+        {
+            int lag = 0;
+            int lead = arr.Length - 1;
+            while (lag < lead)
+            {
+                if (arr[lag] != arr[lead])
+                {
+                    return false;
+                }
+                lag++;
+                lead--;
+            }
+            return true;
+        }
+        public static void SubPalindromes(string str)
+        {
+            List<string> list = new List<string>();
+            int pos = 0;
+            int length = str.Length;
+            while (pos < str.Length)
+            {
+                if (IsPalindrome(str.Substring(pos, length)))
+                {
+                    list.Add(str.Substring(pos, length));
+                }
+                if (length==1)
+                {
+                    pos++;
+                    length = str.Length - pos;
+                }
+                else
+                    length--;
+            }
+            PrintListInLengthOrder(list);
+        }
+        public static void PrintListInLengthOrder(List<string> list)
+        {
+            bool swapped = true;
+            for (int i = 0; i < list.Count - 1 && swapped; i++)
+            {
+                swapped = false;
+                for (int j = 0; j < list.Count; j++)
+                {
+                    if(list[i].Length > list[j].Length)
+                    {
+                        string temp = list[i];
+                        list[i] = list[j];
+                        list[j] = temp;
+                        swapped = true;
+                    }
+                }
+            }
+            foreach(var item in list)
+            {
+                if (item.Length == 1)
+                {
+                    Console.Write(item + " ");
+                }
+                else
+                {
+                    Console.WriteLine(item);
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
